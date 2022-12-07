f950c8812d9bd9beda23a25c02b2be2d9e7a656f
Christy Arys <carys@student.21-school.ru>
2022-12-07 12:44:49 +0300

    06

---
+++ 06/src/ex05/day06_ex05.sql
@@ -0,0 +1,5 @@
+COMMENT ON TABLE person_discounts IS 'Discount table';
+    COMMENT ON COLUMN person_discounts.id IS 'this id';
+    COMMENT ON COLUMN person_discounts.person_id IS 'this id person';
+    COMMENT ON COLUMN person_discounts.pizzeria_id IS 'this id pizzeria';
+    COMMENT ON COLUMN person_discounts.discount IS 'personal discount';
